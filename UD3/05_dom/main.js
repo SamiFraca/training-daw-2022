15b2110e08dc75efac8f6b472fc544f36a5d8e56
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-19 20:59:09 +0200

    19/10/22

--- UD3/05_dom/main.js
+++ UD3/05_dom/main.js
@@ -2,6 +2,9 @@ console.log(document.getElementsByTagName("div"))
 console.log(document.getElementsByClassName("buttons"))
 var elements = document.getElementsByClassName('buttons');
 var requiredElement = elements[0];
+var elements2 = document.getElementsByTagName('h2');
+var requiredElementh2 = elements2[1];
+requiredElementh2.innerHTML="hola"
+console.log(requiredElementh2)
 console.log(elements[0])
-
 console.log(document.querySelectorAll("div.buttons"))
