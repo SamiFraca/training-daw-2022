7ebd5b91994ea52100bb969cbcd50d04af718955
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-25 17:11:09 +0200

    25/10/2022 1

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -2,6 +2,7 @@ window.onload = function (e) {
     console.log('documento cargado')
     document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
     document.querySelectorAll("h2")[1].innerText = "h2 cambiado"
-    // document.getElementsByClassName("first").style.color = "blue"
+    document.querySelectorAll("article")
+    console.log(document.getElementsByTagName("article")[1].getElementsByClassName("first"))
     console.log(document.getElementsByClassName("first"))
 }
