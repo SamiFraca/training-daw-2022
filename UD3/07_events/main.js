d36f6f334e936177751513e894ccc3256ce8e8ec
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-26 20:54:23 +0200

    26/10/22

--- UD3/07_events/main.js
+++ UD3/07_events/main.js
@@ -1,3 +1,13 @@
-window.onload = function(e) {
+window.onload = function (e) {
     console.log('documento cargado')
+}
+console.log(document.getElementsByTagName("p"));
+function ToggleHidden(num) {
+    if (document.getElementById(`contenidos_${num}`).style.display === "none") {
+        document.getElementById(`contenidos_${num}`).style.display = "block"
+        document.getElementById(`enlace_${num}`).innerHTML = "Ocultar"
+    } else {
+        document.getElementById(`contenidos_${num}`).style.display = "none"
+        document.getElementById(`enlace_${num}`).innerHTML = "Mostrar"
+    }
 }
--- UD3/08_forms/main.js
+++ UD3/08_forms/main.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     console.log(document.main_form.elements)
 
     console.log(document.getElementById('pregunta_si').value)
@@ -6,10 +6,21 @@ window.onload = function() {
 
     document.getElementById('pregunta_si').onchange = changeValue
     document.getElementById('pregunta_no').onchange = changeValue
+    var Form = document.main_form
+    console.log(document.main_form)
 
-    
 }
-
+var Button = $('#submit');
+function terms_changed(termsCheckBox){
+    //If the checkbox has been checked
+    if(termsCheckBox.checked){
+        //Set the disabled property to FALSE and enable the button.
+        document.getElementById("submit_button").disabled = false;
+    } else{
+        //Otherwise, disable the submit button.
+        document.getElementById("submit_button").disabled = true;
+    }
+}
 function changeValue(e) {
-     console.log('changed' + e.target.value)
+    console.log('changed' + e.target.value)
 }
