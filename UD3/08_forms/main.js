8f2d324c494eff5f28b3484e2b545f626240bac2
Sami <fracasami@gmail.com>
2022-10-27 12:01:03 +0200

    27/10/22 1st part

--- UD3/08_forms/main.js
+++ UD3/08_forms/main.js
@@ -1,26 +1,34 @@
 window.onload = function () {
-    console.log(document.main_form.elements)
+  //   console.log(document.main_form.elements);
 
-    console.log(document.getElementById('pregunta_si').value)
-    console.log(document.getElementById('pregunta_si').checked)
+  console.log(document.getElementById("pregunta_si").value);
+  //   console.log(document.getElementById("pregunta_si").checked);
 
-    document.getElementById('pregunta_si').onchange = changeValue
-    document.getElementById('pregunta_no').onchange = changeValue
-    var Form = document.main_form
-    console.log(document.main_form)
+  document.getElementById("pregunta_si").onchange = changeValue;
+  document.getElementById("pregunta_no").onchange = changeValue;
+  //   for (let index = 0; index < array.length; index++) {
+  //     const element = array[index];
+  //   }
+};
+// let For = document.main_form.elements;
+// for (let index = 0; index < For.length; index++) {
+//   if (For[index].getElementById("submit"))
+//     () => {
+//       const element = For[index];
+//       console.log(element);
+//     };
+// }
 
+const Privacy = document.getElementById("privacidad");
+const Conditions = document.getElementById("condiciones");
+function terms_changed() {
+  if (Privacy.checked && Conditions.checked) {
+    document.getElementById("submit").disabled = false;
+  } else {
+    document.getElementById("submit").disabled = true;
+  }
 }
-var Button = $('#submit');
-function terms_changed(termsCheckBox){
-    //If the checkbox has been checked
-    if(termsCheckBox.checked){
-        //Set the disabled property to FALSE and enable the button.
-        document.getElementById("submit_button").disabled = false;
-    } else{
-        //Otherwise, disable the submit button.
-        document.getElementById("submit_button").disabled = true;
-    }
-}
+
 function changeValue(e) {
-    console.log('changed' + e.target.value)
+  console.log("changed" + e.target.value);
 }
