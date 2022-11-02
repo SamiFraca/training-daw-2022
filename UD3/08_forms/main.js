012eb3ec4eee11c6c657367c3192f94d976e72a1
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-11-02 20:58:16 +0100

    02/11/22

--- UD3/08_forms/main.js
+++ UD3/08_forms/main.js
@@ -1,31 +1,23 @@
 window.onload = function () {
-  //   console.log(document.main_form.elements);
 
   console.log(document.getElementById("pregunta_si").value);
-  //   console.log(document.getElementById("pregunta_si").checked);
+
 
   document.getElementById("pregunta_si").onchange = changeValue;
   document.getElementById("pregunta_no").onchange = changeValue;
-  //   for (let index = 0; index < array.length; index++) {
-  //     const element = array[index];
-  //   }
-};
-// let For = document.main_form.elements;
-// for (let index = 0; index < For.length; index++) {
-//   if (For[index].getElementById("submit"))
-//     () => {
-//       const element = For[index];
-//       console.log(element);
-//     };
-// }
+  document.getElementById("submit").style.display = "none";
+  Privacy = document.getElementById("privacidad");
+  Conditions = document.getElementById("condiciones");
 
-const Privacy = document.getElementById("privacidad");
-const Conditions = document.getElementById("condiciones");
+};
+
+
 function terms_changed() {
+  document.getElementById("submit").style.display = "none";
   if (Privacy.checked && Conditions.checked) {
-    document.getElementById("submit").disabled = false;
+    document.getElementById("submit").style.display = "block"
   } else {
-    document.getElementById("submit").disabled = true;
+    document.getElementById("submit").style.display = "none"
   }
 }
 
