c10febda9232487f868bedc0d0e70b143619a6e8
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-11-09 20:54:18 +0100

    09/11/2022

--- UD3/09_ajax/fetch/main.js
+++ UD3/09_ajax/fetch/main.js
@@ -1,9 +1,13 @@
-window.onload = function() {
-    console.log('document loaded');
-
-    fetch('http://dummy.restapiexample.com/api/v1/employees')
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-        })
-}
+window.onload = function () {
+    console.log("document loaded");
+  
+    fetch("http://dummy.restapiexample.com/api/v1/employees")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        let tabla = document.getElementById("Tabla");
+        response.data.forEach(item => {
+          tabla.innerHTML += `<tr><td>${item.id}</td><td>${item.employee_name}</td><td>${item.employee_salary}</td><td>${item.employee_age}</td></tr>`;
+        });
+      });
+  };
--- UD3/13_poo_rectangulo/rectangle.js
+++ UD3/13_poo_rectangulo/rectangle.js
@@ -11,25 +11,59 @@ class Rectangle {
     }
 
     // area
-
+    area = () => {
+        return this.x * this.y
+    }
+    // Compare = ( object1, object2) => {
+    //      object1.y <= object2.y && object2.x <= object1.x  ? "Rectangle might interfere.":"All good";
     // overlay
     // tenemos que comprobar la siguiente situación
     // .x----------x.....
     // .|..........|.....
     // .|......x---|----x
     // .x------|---x....|
-    // ........x--------x
+    // ........x--------x6
     // Se superponen si alguna de sus coordenadas están dentro de los límites
     // del otro rectángulo
+
+    overlay = (object2) => {
+        var check = true;
+        let myArray = new Array();
+        let myArray2 = new Array();
+        for (let index = this.x; index <= this.y; index++) {
+            myArray.push(index)
+            console.log(myArray)
+
+        }
+        for (let index = object2.x; index <= object2.y; index++) {
+            myArray2.push(index)
+            console.log(myArray2)
+
+        }
+        for (let i = 0; i <= myArray.length; i++) {
+                if (myArray.includes(myArray2[i])) {
+                    check = false;
+                }
+
+
+            
+        }
+        if (!check) {
+            console.log("The rectangles might interfere.")
+        }else{
+            console.log("All good")
+        }
+    }
 }
 
-let r1 = new Rectangle(5,9);
-let r2 = new Rectangle(3,4);
 
+
+
+let r1 = new Rectangle( 2,9);
+let r2 = new Rectangle(1, 10);
 r1.print()
 r2.print()
+let area = r1.area();
+console.log(`area ${area}`)
 
-// let area = r1.area();
-// console.log(`area ${area}`)
-
-// let isOverlay = r1.overlay(r2)
+let isOverlay = r1.overlay(r2)
