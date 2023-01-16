0129f9fbeec7265ca06a3500f9fed2aa072aa117
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-01-16 18:30:23 +0100

    UD6/6,7,8

---
+++ UD6/07_first_vue_project/first_vue_project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Title></Title>
+    <HelloWorld msg="2323" :counter="1"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/title.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Title
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ UD6/07_first_vue_project/first_vue_project/src/components/title.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="root">
+    <div class="name">Hola que tal, mi nombre es {{ name }} {{ surname }}</div>
+    <div class="years">Mi edad es {{ age }}</div>
+    <div> <button @click="increment">Incrementar</button><button @click="decrement">Decrementar</button></div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  data() {
+    return {
+      name: "Sami",
+      surname: "Fraca",
+      age: 20,
+    };
+     },
+  methods: {
+        increment() {
+          this.age++;
+        },
+        decrement() {
+          this.age <= 0 ? this.age=0 : this.age--;
+        }
+      }
+
+};
+</script>
+<style scoped>
+.name{
+  background: coral;
+}
+.years{
+  background-color: lightcyan;
+}
+</style>
