248f32d2516e5975f6757040d9666576dab936ca
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-15 18:12:09 +0100

    Ej 04_todo UD8

--- UD8/04_todo/src/App.vue
+++ UD8/04_todo/src/App.vue
@@ -5,7 +5,8 @@
     <button @click="addTask">Add</button>
     <div id="content">
       <Task
-        v-for="task in tasks" :key="task.id"
+        v-for="task in tasks"
+        :key="task.id"
         :id="task.id"
         :name="task.name"
         :done="task.done"
@@ -16,25 +17,40 @@
 </template>
 
 <script>
-import Task from "./components/Task.vue"
+import Task from "./components/Task.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Task
+    Task,
   },
   data() {
     return {
-      taskName: '',
+      taskName: "",
       tasks: [
-        { id: 0, name: 'Hacer los deberes', done: false },
-        { id: 1, name: 'Preparar exámen de asignatura :)', done: true },
-      ]
-    }
+        { id: 0, name: "Hacer los deberes", done: false },
+        { id: 1, name: "Preparar exámen de asignatura :)", done: true },
+      ],
+    };
   },
   methods: {
-  }
-}
+    onMark(id) {
+      console.log(`tarea marcada ${id}`);
+      let task = this.tasks.filter((t) => t.id === id)[0];
+      task.done = !task.done;
+    },
+    addTask() {
+      if (this.taskName.trim() !== "") {
+        this.tasks.push({
+        id: this.tasks.length,
+        name: this.taskName,
+        done: false
+        })
+        this.taskName = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
---
+++ UD8/04_todo/src/components/Task.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-if="done">
+      <input type="checkbox" checked @click="cambiarEstado" />
+      <span class="done">{{name}}</span>
+    </div>
+    <div v-if="!done">
+      <input type="checkbox" @click="cambiarEstado" />
+      {{name}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    name: String,
+    done: Boolean,
+  },
+  methods: {
+    cambiarEstado() {
+      console.log("quiero cambiar el estado de " + this.id);
+      this.$emit("mark", this.id);
+    },
+  },
+};
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+  color: grey;
+}
+</style>
